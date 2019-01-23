001321d56fc01658bc5b31ad0db73dfa7afe722e
Nicholas Zaitz <nicholaszaitz@gmail.com>
2019-01-23 15:52:08 -0500

    initial boot project commit, updated .gitignore

---
+++ krowd/src/main/java/com/revature/krowd/KrowdApplication.java
@@ -0,0 +1,14 @@
+package com.revature.krowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrowdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrowdApplication.class, args);
+	}
+
+}
+
