15f02b5bf72621cb87ec290acb586f074ed9dcf5
Nicholas Zaitz <nicholaszaitz@gmail.com>
2019-01-24 10:05:24 -0500

    fixed interface name

--- src/main/java/com/revature/krowdboot/repository/EventCategoryRepository.java
+++ src/main/java/com/revature/krowdboot/repository/EventCategoryRepository.java
@@ -3,7 +3,9 @@ package com.revature.krowdboot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.revature.krowdboot.model.EventCategory;
+
 @Repository
-public interface EventCategory extends JpaRepository<EventCategory, Integer> {
+public interface EventCategoryRepository extends JpaRepository<EventCategory, Integer> {
 
 }
