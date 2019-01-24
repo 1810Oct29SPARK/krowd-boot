ce74dbcc1fe067178cbf1145a4b6cf19b5392d56
Jonathan Snider <jonathan.snider2006@gmail.com>
2019-01-24 11:33:55 -0500

    sending SBG stuff

---
+++ src/main/java/com/revature/krowdboot/driver/EventDriver.java
@@ -0,0 +1,10 @@
+package com.revature.krowdboot.driver;
+
+public class EventDriver {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/main/java/com/revature/krowdboot/repository/EventRepository.java
+++ src/main/java/com/revature/krowdboot/repository/EventRepository.java
@@ -1,11 +1,25 @@
 package com.revature.krowdboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.krowdboot.model.Event;
+import com.revature.krowdboot.model.EventCategory;
+import com.revature.krowdboot.model.User;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-
+	
+	public List<Event> allEvents();
+	public void addEvent(Event e);
+	public void updateEvent(Event e);
+	public void deleteEventById(int id);
+	public Event getEventById(int id);
+	public List<Event> getEventByCategoryId(EventCategory ec);
+	//not sure at the moment how the above and below methods interact with mappings to other tables
+	public List<Event> getEventByUserId(User u);
+	
+	
 }
