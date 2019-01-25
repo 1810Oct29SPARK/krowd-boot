eee43036d89bd66b83dd9e6f40f2268db1e8eecb
Jonathan Snider <jonathan.snider2006@gmail.com>
2019-01-25 13:08:02 -0500

    update 2

--- src/main/java/com/revature/krowdboot/service/EventService.java
+++ src/main/java/com/revature/krowdboot/service/EventService.java
@@ -1,179 +1,59 @@
 package com.revature.krowdboot.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.revature.krowdboot.model.Event;
 import com.revature.krowdboot.model.EventCategory;
 import com.revature.krowdboot.model.User;
 import com.revature.krowdboot.repository.EventRepository;
 
-@Repository
-public class EventService implements EventRepository {
-
-	@Override
-	public List<Event> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Event> findAll(Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Event> findAllById(Iterable<Integer> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> List<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <S extends Event> S saveAndFlush(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteInBatch(Iterable<Event> entities) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Event getOne(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> List<S> findAll(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> List<S> findAll(Example<S> example, Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<Event> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Optional<Event> findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean existsById(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void delete(Event entity) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends Event> entities) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <S extends Event> Optional<S> findOne(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> Page<S> findAll(Example<S> example, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Event> long count(Example<S> example) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <S extends Event> boolean exists(Example<S> example) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<Event> getEventByCategoryId(EventCategory ec) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Event> getEventByUserId(User u) {
-		// TODO Auto-generated method stub
-		return null;
+@Service
+public class EventService {
+	@Autowired
+	EventRepository eventRepository;
+	
+	public List<Event> findallEvents(){
+		List<Event> e= new ArrayList<>();
+		e=eventRepository.findAll();
+		return e;
 	}
 	
-
+	public void addEvent(Event event) {
+		eventRepository.save(event);
+	}
 	
-
+	public void deleteEvent(int id) {
+		Event e=eventRepository.getOne(id);
+		eventRepository.delete(e);
+	}
+	
+	public Event getEventById(int id) {
+		Event e=eventRepository.getOne(id);
+		return e;
+	}
+	
+	public List<Event>getEventByEventCategory(EventCategory ec){
+		List<Event> e=new ArrayList<>();
+		eventRepository.getEventByCategoryId(ec);
+		return e;
+	}
+	
+	public List<Event>getEventByUser(User u){
+		List<Event> e=new ArrayList<>();
+		eventRepository.getEventByUserId(u);
+		return e;
+	}
 }
+	
+
+
