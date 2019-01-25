7a92b22dc78c41fa54aaf83ac864c88cb5cf2c82
Arvester <Arvester.Dickerson@gmail.com>
2019-01-25 10:57:03 -0500

    getAllUsers and getUserById is working

---
+++ src/main/java/com/revature/krowdboot/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.revature.krowdboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.krowdboot.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	UserService userService;
+	
+	//@RequestMapping("/getallusers")
+	
+	
+}
---
+++ src/main/java/com/revature/krowdboot/service/UserService.java
@@ -0,0 +1,38 @@
+package com.revature.krowdboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.krowdboot.model.User;
+import com.revature.krowdboot.repository.UserRepository;
+
+@Service("userService")
+public class UserService {
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> findAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	public void addUser(User user) {
+		 userRepository.addUser(user);
+	}
+	
+	public void updateUserById (User user, int id) {
+		userRepository.updateUserById(user, id);
+	}
+	
+	public void deleteUserById(int id) {
+		userRepository.deleteUserById(id);	}
+	
+	public User getUserById(int id) {
+		return userRepository.getUserById(id);
+	}
+	
+	public User findUserByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+}
