a7500ade165f8637f7d474d981045a4821423234
Jonathan Snider <jonathan.snider2006@gmail.com>
2019-01-26 11:47:06 -0500

    add event working

--- src/main/java/com/revature/krowdboot/service/EventService.java
+++ src/main/java/com/revature/krowdboot/service/EventService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.constraints.NotNull;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
@@ -31,6 +33,9 @@ public class EventService {
 	@Autowired
 	private AddressService addressService;
 	
+	@Autowired
+	private EventCategoryService eventCategoryService;
+	
 	public List<Event> findallEvents(){
 		List<Event> e= new ArrayList<>();
 		e=eventRepository.findAll();
@@ -42,18 +47,23 @@ public class EventService {
 		String picture = (String) json.get("picture");
 		String description = (String) json.get("description");
 		String date = (String) json.get("date");
-		Integer score = (Integer) json.get("score");
-		Integer flag = (Integer) json.get("flag");
+		int score = Integer.parseInt((String) json.get("score"));
+		int flag = Integer.parseInt((String) json.get("flag"));
 		//the above are fine as is, the below need to call methods
-		Integer userId = (Integer) json.get("userId");
+		int userId = Integer.parseInt((String) json.get("userId"));
 		User userObj = userService.getUserById(userId);
 		//we need to call the get userById on the above
+        int eventCategoryId = Integer.parseInt((String) json.get("categoryId"));
+        EventCategory eventCategory = eventCategoryService.getCategoryById(eventCategoryId);
+        //the above gets the Category
 		String streetAddress = (String) json.get("streetAddress");
 		String apartment = (String) json.get("apartment");
 		String city = (String) json.get("city");
 		String state = (String) json.get("state");
-		Integer zipCode = (Integer) json.get("zipCode");
+		int zipCode = Integer.parseInt((String) json.get("zipCode"));
 		Address address = addressService.checkAddress(new Address(streetAddress,apartment,city,state,zipCode));
+		Event e=new Event(name,picture,description,date,address,score,flag,eventCategory,userObj);
+		eventRepository.save(e);
 	}
 	
 	public void deleteEvent(Integer id) {
