a367539c912107faa32546ac48f261683150d2ac
Nicholas Zaitz <nicholaszaitz@gmail.com>
2019-01-26 21:32:06 -0500

    added repository for addresses, finished admin controller, events and users controllers finished pending merges to captain

---
+++ src/main/java/com/revature/krowdboot/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.revature.krowdboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.krowdboot.model.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer>  {
+
+}
