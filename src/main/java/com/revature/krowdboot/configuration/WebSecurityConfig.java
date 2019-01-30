bbccd8dd77e1f50c42a0de6b32e2062385b39ffa
ebanks <e.banks2040@gmail.com>
2019-01-30 14:54:48 -0500

    trimmed the fat

--- src/main/java/com/revature/krowdboot/configuration/WebSecurityConfig.java
+++ src/main/java/com/revature/krowdboot/configuration/WebSecurityConfig.java
@@ -1,24 +1,13 @@
 package com.revature.krowdboot.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import com.ixortalk.aws.cognito.boot.config.JwtAuthenticationProvider;
-import com.revature.krowdboot.security.JwtAuthenticationEntryPoint;
-import com.revature.krowdboot.security.JwtAuthenticationFilter;
-import com.revature.krowdboot.security.JwtAuthenticationSuccessHandler;
-
-
-import java.util.Arrays;
 
 //Protect REST API endpoint with JWT
 
@@ -28,39 +17,14 @@ import java.util.Arrays;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
-
-    @Autowired
-    private JwtAuthenticationProvider authenticationProvider;
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManager() throws Exception {
-
-        return new ProviderManager(Arrays.asList(authenticationProvider));
-    }
-
-    @Bean
-    public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
-        JwtAuthenticationFilter authenticationTokenFilter = new JwtAuthenticationFilter();
-        authenticationTokenFilter.setAuthenticationManager(authenticationManager());
-        authenticationTokenFilter.setAuthenticationSuccessHandler(new JwtAuthenticationSuccessHandler());
-        return authenticationTokenFilter;
-    }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // we don't need CSRF
+
                 .csrf().disable()
-                // change to only some urls
+
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                // Call our errorHandler if authentication fails
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
-                .and()
-                // create session only if required
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
     }
 }
